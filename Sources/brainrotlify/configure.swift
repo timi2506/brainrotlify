f5582000875f74a530cf3d88cb8788bb04c0f659
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-08-19 14:26:59 +0200

    Update configure.swift

--- Sources/brainrotlify/configure.swift
+++ Sources/brainrotlify/configure.swift
@@ -2,8 +2,18 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // this is required to make it available to everyone, not only localhost
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all, // ALL erstmal nur zum Testen
+        allowedMethods: [.GET, .POST, .OPTIONS],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    
+    app.middleware.use(cors)
+    app.middleware.use(ErrorMiddleware.default(environment: app.environment))
+    
     app.http.server.configuration.hostname = ServerConfiguration.hostname
+    
     // set the port to env or the Port defined in Port.swift
     if let portString = Environment.get("PORT"),
        let port = Int(portString) {
